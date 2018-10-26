61dd018b66e29b0a3dcafb154046d4a4fa8aa415
Anil Kumar <anil.kumar3@jungleworks.com>
2018-10-26 09:56:23 +0530

    changes

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express'),
-    socket = require('socket.io'),
-    fs = require('fs'),
-    path = require('path'),
-    app = express(),
-    PORT = process.env.PORT || 3001,
-    server = app.listen(PORT, ()=>{
-        console.log(`server is running on port ${PORT}`);
+socket = require('socket.io'),
+fs = require('fs'),
+path = require('path'),
+app = express(),
+PORT = 3001,
+server = app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
     });
-app.use(express.static(__dirname));
-const io = socket(server);
+    app.use(express.static(__dirname));
+    const io = socket(server);
+
 io.on('connection', (socket)=> {
-    let readstream=fs.readFileSync(path.resolve(__dirname, './image.jpg'));
-    let image64= new Buffer(readstream).toString("base64");
-    socket.emit('img-chunk', image64);
-})
+    socket.on('img-chunk', function(data){
+        var writeFile = fs.writeFile("./"+data.name, data.arrayBuffer, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        }); 
+    });
+});
--- upload.js
+++
@@ -1,6 +0,0 @@
-const socket = io.connect();
-
-socket.on('img-chunk', (chunk)=>{
-    var img = document.getElementById('output');
-    img.setAttribute('src', 'data:image/jpeg;base64,'+chunk);
-})
